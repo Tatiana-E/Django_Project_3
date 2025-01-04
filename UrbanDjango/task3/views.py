2371c88dfde16fb75328382365ab5a4435480572
Tatiana-E <tanya.era1989@gmail.com>
2025-01-04 11:42:43 +0500

    Update views.py

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -13,10 +13,14 @@ def open_games(request):
     game1 = 'Atomik Heart'
     game2 = 'Cyberpunk 2077'
     game3 = 'PayDay2'
+    game4 = 'The Elder Scrolls 6'
+    game5 = 'The Sims 4'
     context = {
         'game1': game1,
         'game2': game2,
         'game3': game3,
+        'game4': game4,
+        'game5': game5,
     }
     return render(request, 'third_task/games.html', context=context)
 
